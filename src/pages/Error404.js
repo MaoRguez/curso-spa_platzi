2f9b4e829e7b0271aea6aabd92b38cd93f444671
Maorguez <avila.mauro@gmail.comz>
2020-04-10 19:03:55 -0500

    cambios de estilos

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -2,6 +2,9 @@ const Error404 = () => {
     const view = `
         <div class="Error404">
             <h2>Error 404</h2>
+            <figure class"Error404-container>
+                <img class="Error404-image" src="image" alt="ooooppps!">
+            </figure>
         </div>
     `
     return view
